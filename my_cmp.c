4d1a34f7ae5d0ab81c9eb0dca80e7bce2472cd08
pau rossell <prossell.particular@gmailcom>
2024-12-20 12:05:24 +0100

    Commit 3: funció my_cmp modificada

--- my_cmp.c
+++ my_cmp.c
@@ -6,7 +6,7 @@
 int main(int argc, char *argv[]) {
 
     if (argc != 3) {
-        perror("Error: Nombre inadequat de parametres\n");
+        perror("Error: Nombre inadequat de paràmetres\n");
         exit(-1);
     }
 
@@ -25,13 +25,13 @@ int main(int argc, char *argv[]) {
     char buffer1, buffer2;
     ssize_t bytes_read1, bytes_read2;
     int bytes_counter = 0,lines_counter = 1;
-    while ((bytes_read1 = read(fd1, &buffer1, 1)) > 0 && (bytes_read1 = read(fd2, &buffer2, 1)) > 0 && fitxers_iguals == 1) {
+    while ((bytes_read1 = read(fd1, &buffer1, 1)) > 0 && (bytes_read2 = read(fd2, &buffer2, 1)) > 0 && fitxers_iguals == 1) {
         bytes_counter ++;
         if (buffer1 =='\n') {
             lines_counter++;
         }
         if (buffer1 != buffer2) {
-            printf("%s %s són diferents: byte %i, linia %i\n",argv[1], argv[2], bytes_counter, lines_counter);
+            printf("%s %s són diferents: byte %i, línia %i\n",argv[1], argv[2], bytes_counter, lines_counter);
             fitxers_iguals = 0;
         }
 
